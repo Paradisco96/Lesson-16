f3e0e017948b4f3f9dc7e23d05f1c8f58a4b4c50
Paradisco96 <mr.wolf.dsgn@gmail.com>
2025-07-03 09:10:37 +0300

    refactor: replace jQuery click handler with vanilla JavaScript for navbar button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
-$('.header__navbar-btn').on('click', function () {
-  $(this).toggleClass('active')
-  $('.header__navbar-list').stop(true, true).slideToggle(300)
-})
+// $('.header__navbar-btn').on('click', function () {
+//   $(this).toggleClass('active')
+//   $('.header__navbar-list').stop(true, true).slideToggle(300)
+// })
+
+const burger = document.querySelector('.header__navbar-btn');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  $(".header__navbar-list").stop(true, true).slideToggle(500);
+});
